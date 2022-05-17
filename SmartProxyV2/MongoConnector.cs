7959efc0a8636fdee45e23950f7bcea9fa555fe1
Disaylt <disofban@gmail.com>
2022-05-17 11:24:28 +0300

    Добавьте файлы проекта.

---
+++ SmartProxyV2/MongoConnector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace SmartProxyV2
+{
+    internal static class MongoConnector
+    {
+        private const string _connectionString = "mongodb://10.100.1.250:27017";
+        private const string _databaseName = "SmartProxyV2";
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+
+        static MongoConnector()
+        {
+            _client = new MongoClient(_connectionString);
+            _database = _client.GetDatabase(_databaseName);
+        }
+
+        internal static IMongoCollection<BsonDocument> GetCollection(string collectionName)
+        {
+            IMongoCollection<BsonDocument> collection = _database.GetCollection<BsonDocument>(collectionName);
+            return collection;
+        }
+    }
+}
