21a6d6625fdaaa64cd10f9de4e09bac9a07f6076
Disaylt <disofban@gmail.com>
2022-05-17 12:18:58 +0300

    add proxyData

---
+++ SmartProxyV2/ProxyData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartProxyV2
+{
+    public class ProxyData
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Ip { get; set; }
+        public string Port { get; set; }
+    }
+}
