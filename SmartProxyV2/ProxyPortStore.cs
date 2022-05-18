6a3bec9c998bcfcdc110b708e7e63ed5e43b6518
Disaylt <disofban@gmail.com>
2022-05-18 19:09:28 +0300

    divaded Port controller and PortStore

---
+++ SmartProxyV2/ProxyPortStore.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using SmartProxyV2.MongoModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartProxyV2
+{
+    internal static class ProxyPortStore
+    {
+        private const string _collectionName = "ProxyPort";
+        public static IMongoCollection<PortMongoModel> Collection { get; }
+
+        static ProxyPortStore()
+        {
+            Collection = MongoConnector.GetCollection<PortMongoModel>(_collectionName);
+        }
+    }
+}
--- SmartProxyV2/SmartProxyHandler.cs
+++ SmartProxyV2/SmartProxyHandler.cs
@@ -8,6 +8,18 @@ namespace SmartProxyV2
 {
     public static class SmartProxyHandler
     {
-        
+        private readonly static ProxyPortController _portStore;
+        private readonly static ProxyStore _proxyStore;
+
+        static SmartProxyHandler()
+        {
+            _portStore = new ProxyPortController();
+            _proxyStore = new ProxyStore();
+        }
+
+        public static void ClosePort(int port, string type)
+        {
+            
+        }
     }
 }
