f8e4f4cbf84cadbac484b9b3ef955c40f430c637
Disaylt <disofban@gmail.com>
2022-05-23 17:55:21 +0300

    add settings store

---
+++ SmartProxyV2/SmartProxySettingsStore.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using SmartProxyV2.MongoModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartProxyV2
+{
+    internal class SmartProxySettingsStore
+    {
+        private const string _collectionName = "Settings";
+        private static IMongoCollection<SmartProxySettingsMongoModel> _collection;
+        internal static IMongoCollection<SmartProxySettingsMongoModel> Collection
+        {
+            get
+            {
+                if (_collection == null)
+                {
+                    _collection = MongoConnector.GetCollection<SmartProxySettingsMongoModel>(_collectionName);
+                }
+                return _collection;
+            }
+        }
+    }
+}
