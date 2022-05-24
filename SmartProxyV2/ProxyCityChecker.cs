51ecbc900741f3f20ca15c8fdc11f714b372a980
Disaylt <disofban@gmail.com>
2022-05-24 12:39:03 +0300

    add proxyCityChecker

--- SmartProxyV2/ProxyCityChecker.cs
+++ SmartProxyV2/ProxyCityChecker.cs
@@ -1,27 +1,38 @@
 ﻿using MongoDB.Driver;
+using Newtonsoft.Json.Linq;
+using SmartProxyV2.JsonModels;
+using SmartProxyV2.Models;
 using SmartProxyV2.MongoModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SmartProxyV2
 {
-    internal class ProxyChecker
+    internal class ProxyCityChecker
     {
-        private readonly int _port;
+        protected readonly ProxyModel ProxyModel;
 
-        internal ProxyChecker(int port)
+        internal ProxyCityChecker(ProxyModel proxy)
         {
-            _port = port;
+            ProxyModel = proxy;
         }
 
         internal async Task<bool> ProxyFromCity(string city)
         {
-            using (var httpClient = new HttpClient())
+            ProxyJsonModel proxyInfo = await GetProxyInfo();
+        }
+
+        private async Task<ProxyJsonModel> GetProxyInfo()
+        {
+            HttpClientHandler httpMessageHandler = new HttpClientHandler();
+            using (var httpClient = new HttpClient(httpMessageHandler))
             {
+                
                 using (var request = new HttpRequestMessage(new HttpMethod("GET"), "http://ip-api.com/json"))
                 {
                     request.Headers.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0");
@@ -31,7 +42,9 @@ namespace SmartProxyV2
                     request.Headers.TryAddWithoutValidation("Connection", "keep-alive");
                     request.Headers.TryAddWithoutValidation("Upgrade-Insecure-Requests", "1");
 
-                    var response = await httpClient.SendAsync(request);
+                    var response = await httpClient.SendAsync(request).Result.Content.ReadAsStringAsync();
+                    ProxyJsonModel proxyInfo = JToken.Parse(response).ToObject<ProxyJsonModel>();
+                    return proxyInfo;
                 }
             }
         }
